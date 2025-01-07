f153aa5d4649c28a9c2659c28c95326fcafd81bc
Gautam Sharma <gautam2586272@gmail.com>
2025-01-07 11:48:55 +0530

    frontend init && react-router-dom && user context provider

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Route, BrowserRouter, Routes} from 'react-router-dom'
+import Home from '../components/Home'
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home />}></Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+export default AppRoutes
